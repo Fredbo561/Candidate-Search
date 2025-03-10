3315f6cf947a9995e9fa85352d74f6b86a915035
Fredbo561 <fredbo561@gmail.com>
2025-03-10 02:28:51 -0400

    working

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <>
+    <div className='app-container'>
       <Nav />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
